5b41fe946b4e2cb1b6bcf9d0e2b1e3bb3e4ce684
mubbashirhussain <mubbashirhhussain@gmail.com>
2025-05-05 23:39:00 +0500

    update Theme colors

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -5,7 +5,7 @@ import { Toaster } from "sonner";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+    <ThemeProvider attribute="class" defaultTheme="light">
       <Toaster richColors position="top-right" />
       {children}
     </ThemeProvider>
