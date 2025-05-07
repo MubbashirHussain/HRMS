b49f76ee2eea78f05dfe4610bf87048e950826c1
mubbashirhussain <mubbashirhhussain@gmail.com>
2025-05-07 20:44:48 +0500

    Add employee management features with CRUD operations and dummy data

---
+++ src/constaint/dummy/employees.ts
@@ -0,0 +1,44 @@
+import { Employee } from "@/app/(authenticated)/employees/employee";
+
+export const EmployeedummyData : Employee[]= [
+    {
+      id: "1",
+      name: "John Doe",
+      email: "john.doe@example.com",
+      department: "Engineering",
+      joiningDate: "2022-01-15",
+      status: "Active",
+    },
+    {
+      id: "2",
+      name: "Jane Smith",
+      email: "jane.smith@example.com",
+      department: "Marketing",
+      joiningDate: "2021-11-01",
+      status: "Active",
+    },
+    {
+      id: "3",
+      name: "Bob Johnson",
+      email: "bob.johnson@example.com",
+      department: "Sales",
+      joiningDate: "2023-03-20",
+      status: "Pending",
+    },
+    {
+      id: "4",
+      name: "Alice Brown",
+      email: "alice.brown@example.com",
+      department: "HR",
+      joiningDate: "2022-08-01",
+      status: "Inactive",
+    },
+    {
+      id: "5",
+      name: "Michael Davis",
+      email: "michael.davis@example.com",
+      department: "Engineering",
+      joiningDate: "2023-05-10",
+      status: "Active",
+    },
+  ];
